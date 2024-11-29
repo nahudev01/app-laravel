bcb5ac7c0d86551922e92a64b337e5d81c548b4c
ngarcia-dev <garcianahuel77@gmail.com>
2024-11-29 08:26:15 -0300

    created: evento para pasar el chirp recien creado a la notification y envio del evento en el modelo Chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,14 +2,22 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 
 class Chirp extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'message'
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     public function user(): BelongsTo
